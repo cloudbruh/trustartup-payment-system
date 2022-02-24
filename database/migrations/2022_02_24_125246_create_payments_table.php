a3e25c6950e6aa75c69ef0a0a7bf5bfac9ddb866
vanchenkin <vanchenkin@yandex.ru>
2022-02-24 12:58:19 +0300

    initial commit

---
+++ database/migrations/2022_02_24_125246_create_payments_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePaymentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id')->unsigned();
+            $table->integer('startup_id')->unsigned();
+            $table->unsignedDecimal('amount', $precision = 15, $scale = 2);
+            $table->enum('status', ['CREATOR', 'PENDING', 'SUCCESS', 'FAILURE']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('payments');
+    }
+}
